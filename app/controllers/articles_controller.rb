dec4c5bf009444fc0a123c739582b433107d62ad
Guillaume Laine <guillaumelaine2.0@gmail.com>
2017-01-07 23:53:56 +0100

    added update and edit actions with validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,28 @@ class ArticlesController < ApplicationController
 
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
 
