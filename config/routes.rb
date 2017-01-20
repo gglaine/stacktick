ff1166fd5425acaae759eda75b8f02d2cde76164
Guillaume Laine <guillaumelaine2.0@gmail.com>
2017-01-21 00:30:50 +0100

    changed root route to article#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
