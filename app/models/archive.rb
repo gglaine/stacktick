364e12aacc3d6a2058f998b5412421cd674c7b2c
Guillaume Laine <guillaumelaine2.0@gmail.com>
2017-09-28 15:09:05 +0200

    mods to archive model

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,2 +1,12 @@
 class Archive < ApplicationRecord
+  include HTTParty
+
+  base_uri 'http://fresques.ina.fr/jalons/api/search?query=nature&limit=5&offset=5'
+  default_params fields: "description", q: "search"
+  format :json
+
+  def self.for term
+    get("", query: { query: term })["elements"]
+  end
+
 end
