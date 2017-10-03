98ae41e2b380c8a267d5ff9f7cd2a3832f3bd3c9
Guillaume Laine <guillaumelaine2.0@gmail.com>
2017-10-03 21:04:39 +0200

    change color variables

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'contact@littlebigblog.com'
+  default from: 'contact@techkilla.com'
   layout 'mailer'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928125258) do
+ActiveRecord::Schema.define(version: 20171003152145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170928125258) do
     t.string   "author"
     t.string   "publisher"
     t.string   "source"
+    t.string   "url"
   end
 
   create_table "attachinary_files", force: :cascade do |t|
