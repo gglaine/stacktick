42094124cff4a4a4098685ec08a475f8185305a5
Guillaume Laine <guillaumelaine2.0@gmail.com>
2017-10-07 18:38:50 +0200

    no change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,6 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
     if @article.save
       redirect_to @article
     else
--- app/models/article.rb
+++ app/models/article.rb
@@ -25,4 +25,6 @@ class Article < ApplicationRecord
 
   acts_as_votable
 
+
+
 end
