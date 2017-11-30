0a53f57ee2a61c77548130db451b98a30d9df633
Guillaume Laine <guillaumelaine2.0@gmail.com>
2017-11-30 15:04:01 +0100

    design

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
   def get_latest_hn
-    @hackernews = Hackernews.all
+    @hackernews = Hackernews.all.order('id DESC')
     @hackernews = Kaminari.paginate_array(@hackernews).page(params[:page]).per(20)
   end
 end
