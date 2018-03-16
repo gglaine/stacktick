cff8635c21838d2e55dd8e2c8f0e3fa134b0fccd
Guillaume Laine <guillaumelaine2.0@gmail.com>
2018-03-16 11:26:00 +0100

    git reset, delete redundancies

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,21 @@
 Rails.application.routes.draw do
 
-
-
-  get 'wagonposts/index'
-
-  get 'wagonposts/create'
-
-  get 'archives/index'
-
-  get 'users/show'
-
-  get 'techmemes', to: 'techmemes#index'
-
-  get 'hackernews', to: 'hackernews#index'
-
-  get 'techcrunches', to: 'techcrunches#index'
-
-  get 'lewagon/blog', to: 'wagonsposts#index'
+  root 'articles#index'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount Attachinary::Engine => "/attachinary"
 
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  end
 
   resources :users
 
-  resources :archives
+  get 'archives/index'
 
-  resources :articles do
-    resources :comments
-  end
-
-  resources :categories
-
-  get 'courses/index'
-
-  get 'welcome/index'
-
-  root 'articles#index'
-  #
-  # root 'welcome#scrape_hn'
-
-  # get'application/scrape' to: 'application#scrape'
+  get 'techmemes', to: 'techmemes#index'
+  get 'hackernews', to: 'hackernews#index'
+  get 'techcrunches', to: 'techcrunches#index'
+  get 'lewagon/blog', to: 'wagonsposts#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/migrate/20170928125258_create_archives.rb
+++
@@ -1,8 +0,0 @@
-class CreateArchives < ActiveRecord::Migration[5.0]
-  def change
-    create_table :archives do |t|
-
-      t.timestamps
-    end
-  end
-end
